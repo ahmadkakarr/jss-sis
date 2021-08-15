1cc3239ebf63b2612f37416ffeed056a5d8534a9
AhmadKakarr <ahmadkakarr@gmail.com>
2021-08-15 22:35:15 +0500

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f5e197a58ab69f428666d4fdbf91c2d26e94509b',
+        'name' => 'francoisjacquet/rosariosis',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'francoisjacquet/rosariosis' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f5e197a58ab69f428666d4fdbf91c2d26e94509b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
